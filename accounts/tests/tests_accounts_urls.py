c7e03f2a0f81b424d3c983e019f23d601d8bb3bb
walexhenrique <walex999067@gmail.com>
2022-09-20 23:33:48 -0300

    Create view login

---
+++ accounts/tests/tests_accounts_urls.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+
+from django.urls import reverse
+
+
+class AccountsURLsTest(TestCase):
+    def test_login_url_path_is_correct(self):
+        """
+        This test verifies that the path points to the correct location
+        """
+        
+        url = reverse('accounts:login')
+        self.assertEqual(url, '/accounts/login/')
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponse, render
+
 
 # Create your views here.
+def login(request):
+    return render(request, 'accounts/login.html')
