e96b74a9e1675584efaaca31f80f560a723cc7a2
walexhenrique <walex999067@gmail.com>
2022-09-23 03:03:12 -0300

    Add test username exists

--- accounts/tests/tests_register_form.py
+++ accounts/tests/tests_register_form.py
@@ -1,5 +1,6 @@
 from accounts.forms.register_form import RegisterForm
 from django.test import TestCase
+from django.urls import reverse
 
 
 class RegisterFormTest(TestCase):
@@ -16,5 +17,29 @@ class RegisterFormTest(TestCase):
         for field, label_about in fields_labels:
             with self.subTest(field=field, label_about=label_about):
                 self.assertEqual(form.fields[field].label, label_about)
+    
+    def test_username_alredy_exists_in_bd(self):
+        url = reverse('accounts:register_create_view')
+        form_data = {
+            'username': 'jorge',
+            'first_name': 'jorgezito',
+            'last_name': 'cabral',
+            'email': 'jorge@cabral.com',
+            'password': '123456',
+            'password2': '123456',
+        }
+        user_exists_msg = 'Username alredy exists in database'
+        # Create account and after try create another account with equals username
+        
+        self.client.post(url, data=form_data, follow=True)
+        form_data['email'] = 'jorge1@cabral.com'
+
+        response = self.client.post(url, data=form_data, follow=True)
+
+        self.assertIn(user_exists_msg, response.context['form'].errors.get('username'))
+        
+
+        
+
 
 
