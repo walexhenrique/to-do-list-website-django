6e568b0d61197f8184baaa29cd18d0d356173ad8
walexhenrique <walex999067@gmail.com>
2022-09-24 17:21:36 -0300

    Add pagination and limit per page in dashboard view

--- platform_users/views.py
+++ platform_users/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from django.shortcuts import render
-from django.urls import reverse
 
 from platform_users.models import Task
 
@@ -9,8 +9,23 @@ from platform_users.models import Task
 @login_required(login_url='accounts:login_view')
 def dashboard(request):
     user = request.user
-    print(user)
-    tasks = Task.objects.filter(user=user)
-    print(tasks)
     
-    return render(request, 'platform_users/dashboard.html', {'tasks': tasks})
+    tasks = Task.objects.filter(user=user)
+    limits = ['5', '8', '10']
+
+    limit = request.GET.get('limit', '5')
+    if not limit in limits:
+        limit = limits[0]
+
+    paginator = Paginator(tasks, limit)
+
+    page_number = request.GET.get('page', '1')
+    
+    page_obj = paginator.get_page(page_number)
+    
+    
+    return render(request, 'platform_users/dashboard.html', {
+        'tasks': page_obj,
+        'quantity_per_page': limits,
+        'limit': limit,
+        })
