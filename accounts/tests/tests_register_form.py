4b614cc5a77d05cb4436b5690cfe28d0f23a3774
walexhenrique <walex999067@gmail.com>
2022-09-23 21:45:45 -0300

    Add tests RegisterView and RegisterForm

--- accounts/tests/tests_register_form.py
+++ accounts/tests/tests_register_form.py
@@ -37,9 +37,89 @@ class RegisterFormTest(TestCase):
         response = self.client.post(url, data=form_data, follow=True)
 
         self.assertIn(user_exists_msg, response.context['form'].errors.get('username'))
+    
+    def test_email_alredy_exists_in_bd(self):
+        url = reverse('accounts:register_create_view')
+        form_data = {
+            'username': 'jorge',
+            'first_name': 'jorgezito',
+            'last_name': 'cabral',
+            'email': 'jorge@cabral.com',
+            'password': '123456',
+            'password2': '123456',
+        }
+        email_exists_msg = 'Email alredy exists in database'
+
+        self.client.post(url, data=form_data, follow=True)
+        form_data['username'] = 'batista'
+        response = self.client.post(url, data=form_data, follow=True)
+
+        self.assertIn(email_exists_msg, response.context['form'].errors.get('email'))
+
+    def test_password_least_4_letters_not_valide(self):
+        url = reverse('accounts:register_create_view')
+
+        form_data = {
+            'username': 'jorge',
+            'first_name': 'jorgezito',
+            'last_name': 'cabral',
+            'email': 'jorge@cabral.com',
+            'password': '123',
+            'password2': '123',
+        }
+
+        password_is_short_msg = 'Error, Password is too short'
+
+        response = self.client.post(url, data=form_data, follow=True)
+
+        self.assertIn(password_is_short_msg, response.context['form'].errors.get('password'))
         
+    def test_password2_least_4_letters_not_valide(self):
+        url = reverse('accounts:register_create_view')
 
+        form_data = {
+            'username': 'jorge',
+            'first_name': 'jorgezito',
+            'last_name': 'cabral',
+            'email': 'jorge@cabral.com',
+            'password': '1234',
+            'password2': '123',
+        }
+
+        password2_is_short_msg = 'Error, Password is too short'
+        response = self.client.post(url, data=form_data, follow=True)
+
+        self.assertIn(password2_is_short_msg, response.context['form'].errors.get('password2'))
+
+    def test_password_and_password2_not_equals(self):
+        url = reverse('accounts:register_create_view')
+
+        form_data = {
+            'username': 'jorge',
+            'first_name': 'jorgezito',
+            'last_name': 'cabral',
+            'email': 'jorge@cabral.com',
+            'password': '12345',
+            'password2': '1234',
+        }
+
+        password_and_password2_not_equals_msg = 'Error, Passwords not equals'
+        response = self.client.post(url, data=form_data, follow=True)
+        self.assertIn(password_and_password2_not_equals_msg, response.context['form'].errors.get('__all__'))
         
+    def test_register_user_valid_works_for_create_new_user_in_db(self):
+        form_data = {
+            'username': 'jorge',
+            'first_name': 'jorgezito',
+            'last_name': 'cabral',
+            'email': 'jorge@cabral.com',
+            'password': '12345',
+            'password2': '12345',
+        }
+        form = RegisterForm(form_data)
+        user = form.save(commit=False)
+        user.set_password(user.password)
+        user.save()
 
-
-
+        user_exists = self.client.login(username='jorge', password='12345')
+        self.assertTrue(user_exists)
--- accounts/tests/tests_register_view.py
+++ accounts/tests/tests_register_view.py
@@ -1,4 +1,5 @@
 from accounts.views import register_view
+from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import resolve, reverse
 
@@ -19,4 +20,49 @@ class RegisterViewTest(TestCase):
         url = reverse('accounts:register_view')
         func_view = resolve(url)
         self.assertEqual(func_view.func, register_view)
+    
+    def test_view_register_create_returns_error_404_if_method_not_equals_POST(self):
+        url = reverse('accounts:register_create_view')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 404)
+    
+    def test_view_register_create_redirect_if_user_successfully_created(self):
+        url = reverse('accounts:register_create_view')
+        form_data = {
+            'username': 'jorge',
+            'first_name': 'jorgezito',
+            'last_name': 'cabral',
+            'email': 'jorge@cabral.com',
+            'password': '123456',
+            'password2': '123456',
+        }
         
+        response = self.client.post(url, data=form_data, follow=True)
+        redirect_url = reverse('accounts:login_view')
+        self.assertRedirects(response, redirect_url)
+
+        # verify user created
+        user = User.objects.filter(username=form_data['username']).exists()
+
+        self.assertTrue(user)
+        
+    def test_view_register_create_redirect_to_register_if_invalid_user_created(self):
+        url = reverse('accounts:register_create_view')
+        form_data = {
+            'username': 'jorge',
+            'first_name': 'jorgezito',
+            'last_name': 'cabral',
+            'email': 'jorge@cabral.com',
+            'password': '1234',
+            'password2': '12345',
+        }
+        
+        response = self.client.post(url, data=form_data, follow=True)
+        redirect_url = reverse('accounts:register_view')
+        self.assertRedirects(response, redirect_url)
+
+        # User can't created
+        user = User.objects.filter(username=form_data['username']).exists()
+
+        self.assertFalse(user)
